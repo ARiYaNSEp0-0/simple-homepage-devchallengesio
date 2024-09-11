07fb9bbaae3b0454edd9af95c4397b8c31449bc7
AriyanSep0_0 <ariansepahi825121314151617@gmail.com>
2024-09-11 13:27:10 +0330

    Add fonts & extend tailwind.config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,29 @@
 /** @type {import('tailwindcss').Config} */
+const defaultTheme = require("tailwindcss/defaultTheme");
+
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      
+      colors: {
+        blue: {
+          50: "#f2f9fe",
+          700: "#2a4dd0",
+          900: "#223344",
+          950: "#111729",
+        },
+        green: {
+          500: "#4ca154",
+        },
+        stone: {
+          400: "#909193",
+        },
+      },
+      fontFamily: {
+        sans: ['"Poppins"', ...defaultTheme.fontFamily.sans],
+      },
+    },
   },
   plugins: [],
-}
+};
